75bd608782202039c7e4cf6435c85d8a830bc57a
captainbyliner <mcarthurdonald@gmail.com>
2014-03-02 23:14:19 -0500

    lots of changes and it should all be busted. Lost.

--- application.js
+++ application.js
@@ -1,25 +1,85 @@
 
 
-$(document).ready(function(){
+var questions = [{
+	text: 'How many majors did Jack Nicklaus win?',
+	answers: ['18 Majors', '14 Majors', '19 Majors', '16 Majors'],
+	correctAnswer: 0
+},{
+	text: 'What is the highest recorded Par 4 score in a PGA Tour event?',
+	answers: ['12', '16', '15', '18'],
+	correctAnswer:1
+},{
+	text: 'Who has won the most British Open titles?',
+	answers: ['Tiger Woods', 'Harry Vardon', 'Tom Watson', 'Old Tom Morris'],
+	correctAnswer:1
+},{
+	text: 'Who said "The only thing a golfer needs is more sunlight?"',
+	answers: ['Raymond Floyd', 'Tom Watson', 'Bagger Vance', 'Ben Hogan'],
+	correctAnswer:3
+},{
+	text: 'What is the maximum number of clubs a golfer can carry?',
+	answers:['12', '15', '14', '16'],
+	correctAnswer:2
+}];
+
+var questionKey = 0;
+
+function displayQuestion(key) {
+
+	var question = questions[key];
+
+	$("#questionDeck").text(question.text);
+	$("#answerWrapper").css("background-color", "black");
+	$("#answerWrapper").text(question.answers);
+
+
+	$("#answers").empty();
+
+	for (var answerKey = 0, totalAnswers = question.answers.length);
+		answerKey < totalAnswers;
+		answerKey++
+
+    {
+    	var answer = question.answers[answerKey];
+
+ 	$("#answers").append("<li><input type='radio' name ='answers'" + 
+ 						 "value='" + answerKey + "')/> " + answer + "</li>");
+        
+	}
+}
+
+$(document).ready(function() {
+
+	displayQuestion(questionKey);
+})
+
+	$("#addAnswer").on('click', function (){
+
+		questionKey--;
+		if (questionKey < 0) {
+			questionKey = 0;
+		}
+
+		displayQuestion(questionKey);
+
+	});
+
 
    
+   $("#addAnswer).on('click', function() {
+   		var selectedAnswer = $("#answers input[type='radio']:checked").val;
+   })
+
+   
+    
+ 	var question = questions[questionKey];
+
+ 		if (selectedAnswer == questions.correctAnswer) {
+ 			console.log("You are correct");
+ 		} else {
+ 			console.log("Nope, you are wrong");
+ 		}
+   
+    });    
 });
-
-
-$(".panelDeck").fadeIn("slow");
-$("#playButton").fadeIn("slow");
-$("#playButton").css("display" , "block");
-
-$("#playButton").click(function(){
-    $(".panel").remove();
-    $(".panel1").show();
-});
-
-$("#addAnswer").click(function() {
-    $(".panel1").remove();
-    $(".panel2").show();
-});
-
-
- 
 
